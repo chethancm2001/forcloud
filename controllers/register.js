27761e73d6398e361b7a783ab79de455ae5b70e0
chethancm2001 <chethancm2001kadur@gmail.com>
2022-08-30 16:48:25 +0530

    added joi package and updated save method

--- controllers/register.js
+++ controllers/register.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose')
 const userModel = require('../models/user')
 const Joi = require('joi')
 
@@ -16,25 +15,24 @@ async function register(req,res){
 let {name, email,password} = req.body
 
 let result = Schema.validate({name, email,password})
+
 if(result.error != null){
    
-   return res.json({len: result.error.details.length})
+   return res.json({len: result.error.details[0].message})
 }
 
- return res.send(result)
 let user = new userModel({name,email,password})
 try{
 let result = await user.save()
-if(result != null){
-   res.json(result)
+return res.send(result)
 }
-else{
-    res.send('error')
-}
+catch(exp){
 
-}
-catch(err){
-res.send(err)
+    if(exp.code == '11000'){
+        return res.status(409).json({"message":"email is already prasent"})
+         }
+        
+     return res.status(409).json({message:"failed to save retry"})
 }
 }
 module.exports = register
