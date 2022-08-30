6a2f5673b377595f5cf738f56296a662aa5e8090
chethancm2001 <chethancm2001kadur@gmail.com>
2022-08-30 13:37:51 +0530

    added router

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
-
+const router = require("./routes/routes")
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use('/',router)
 
 
-app.get('/',(req,res)=>{
-    res.send("runing sucessfully")
-})
+
 const port = process.env.PORT || 3000
 
 app.listen(port,(err)=>{
