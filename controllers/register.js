9eeb9ee18937ec8d9f1963573c72b9783a96481b
chethancm2001 <chethancm2001kadur@gmail.com>
2022-08-30 16:32:53 +0530

    added joi package

--- controllers/register.js
+++ controllers/register.js
@@ -1,7 +1,27 @@
 const mongoose = require('mongoose')
 const userModel = require('../models/user')
+const Joi = require('joi')
+
+
+
+
+    let Schema = Joi.object().keys({
+        name:Joi.string().min(3).required(),
+        email:Joi.string().min(3).email().required(),
+        password:Joi.string().min(3).required()
+    })
+
+
 async function register(req,res){
 let {name, email,password} = req.body
+
+let result = Schema.validate({name, email,password})
+if(result.error != null){
+   
+   return res.json({len: result.error.details.length})
+}
+
+ return res.send(result)
 let user = new userModel({name,email,password})
 try{
 let result = await user.save()
